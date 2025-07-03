e6b7d5b74ead6118d641cf8da22e5d73093870d4
Nguyen93-Arena <vannguyenbp230393@gmail.com>
2025-07-03 21:09:39 +0700

    Update firebase-config.js

--- frontend/firebase-config.js
+++ frontend/firebase-config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIza...YOUR_API_KEY",
+  apiKey: "AIza...pppjonifp6ycdjeck3gk10bsvjoyu0kkycb4fpbusyhotkaueqrah7ygj6m0hwoq",
   authDomain: "arena-pi.firebaseapp.com",
   databaseURL: "https://arena-pi-default-rtdb.firebaseio.com",
   projectId: "arena-pi",
   storageBucket: "arena-pi.appspot.com",
   messagingSenderId: "YOUR_ID",
-  appId: "YOUR_APP_ID"
+  appId: "1:93311684976:web:a892e68a0e44ba5660ffb5"
 };
 
 const app = initializeApp(firebaseConfig);
