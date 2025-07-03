bf0368334947f1096291007efb63d977299562cc
Nguyen93-Arena <vannguyenbp230393@gmail.com>
2025-07-03 10:36:46 +0700

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         window.Pi.init
       ) {
         try {
-          window.Pi.init({ version: "2.0", sandbox: false }); // ✅ MAINNET
+          window.Pi.init({ version: "2.0", sandbox: true })
           setPi(window.Pi);
           setStatus("✅ Pi SDK đã sẵn sàng.");
         } catch (err) {
